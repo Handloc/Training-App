36edbdd7ad4bc39c1ecaddca7c518fc35c20edff
Maciej Handke <maciejhandke11@gmail.com>
2023-05-31 23:16:14 +0200

    Pass 'register' and 'errors' as props to Field component

--- src/components/AuthForm/AuthForm.tsx
+++ src/components/AuthForm/AuthForm.tsx
@@ -1,11 +1,37 @@
+import { useForm } from "react-hook-form";
 import AuthFormField from "./AuthFormField";
 
 const AuthForm: React.FC<{}> = () => {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm();
+
+  const HandlerSubmit = (e: any) => {
+    console.log(e);
+  };
+
   return (
-    <form>
-      <AuthFormField field_name="e-mail" type="text" />
-      <AuthFormField field_name="password" type="password" />
-      <button>Sign in</button>
+    <form
+      onSubmit={handleSubmit(HandlerSubmit)}
+      className="border-cyan-600 border-2 flex flex-col w-4/5 m-auto mt-40 items-center p-5"
+    >
+      <AuthFormField
+        field_name="E-mail"
+        type="text"
+        register={register}
+        errors={errors}
+      />
+      <AuthFormField
+        field_name="Password"
+        type="password"
+        register={register}
+        errors={errors}
+      />
+      <button className="mt-4 border-cyan-600 border-2 pl-4 pr-4">
+        Sign in
+      </button>
     </form>
   );
 };

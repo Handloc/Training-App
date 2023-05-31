c4d76f399343028c7409e50930cdae54ab707cff
Maciej Handke <maciejhandke11@gmail.com>
2023-05-31 23:17:24 +0200

    Display validation errors

--- src/components/AuthForm/AuthFormField.tsx
+++ src/components/AuthForm/AuthFormField.tsx
@@ -1,24 +1,33 @@
-import { useForm } from "react-hook-form";
+import {
+  UseFormRegister,
+  FieldValues,
+  DeepMap,
+  FieldError,
+} from "react-hook-form";
 
-const AuthFormField: React.FC<{ field_name: string; type: string }> = ({
-  field_name,
-  type,
-}) => {
-  const {
-    register,
-    formState: { errors },
-  } = useForm();
+const AuthFormField: React.FC<{
+  field_name: string;
+  type: string;
+  register: UseFormRegister<FieldValues>;
+  errors: DeepMap<FieldValues, FieldError>;
+}> = ({ field_name, type, register, errors }) => {
+  console.log(errors);
 
   return (
-    <div>
-      <label htmlFor={field_name}>{field_name}</label>
+    <div className="flex flex-col">
+      {/* <label htmlFor={field_name}>{field_name}</label> */}
       <input
         type={type}
+        className="border-b-2 border-cyan-600"
+        placeholder={field_name}
         {...register(field_name, {
-          required: { value: true, message: `${field_name} field is required` },
+          required: {
+            value: true,
+            message: `You must enter your ${field_name}`,
+          },
         })}
       />
-      <p>{errors.field_name?.message?.toString()}</p>
+      <p className="text-red-600">{errors[field_name]?.message}</p>
     </div>
   );
 };

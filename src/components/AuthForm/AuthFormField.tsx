3cdc1845218f16dee5cef76f3ed90769c6e38f58
Maciej Handke <maciejhandke11@gmail.com>
2023-06-04 18:42:24 +0200

    Add id property for input field

--- src/components/AuthForm/AuthFormField.tsx
+++ src/components/AuthForm/AuthFormField.tsx
@@ -8,25 +8,27 @@ import {
 const AuthFormField: React.FC<{
   field_name: string;
   type: string;
+  id: string;
   register: UseFormRegister<FieldValues>;
   errors: DeepMap<FieldValues, FieldError>;
-}> = ({ field_name, type, register, errors }) => {
+}> = ({ field_name, type, register, errors, id }) => {
   return (
     <div className="flex flex-col w-4/5 mb-3">
       <input
         type={type}
+        id={id}
         className={`${
-          errors[field_name]?.message ? "border-red-600" : "border-amber-400"
+          errors[id]?.message ? "border-red-600" : "border-amber-400"
         } border-b-2 bg-zinc-600 valid:bg-zinc-600 focus:outline-none focus:border-fuchsia-600`}
         placeholder={field_name}
-        {...register(field_name, {
+        {...register(id, {
           required: {
             value: true,
             message: `${field_name} field cannot be empty`,
           },
         })}
       />
-      <p className="text-red-600 text-center">{errors[field_name]?.message}</p>
+      <p className="text-red-600 text-center">{errors[id]?.message}</p>
     </div>
   );
 };

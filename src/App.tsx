2d96c56d4f1c13caedbab267fe367ee094322bd6
Maciej Handke <maciejhandke11@gmail.com>
2023-07-02 22:42:55 +0200

    Add route to Home Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import AuthPage from "./pages/AuthPage";
+import HomePage from "./pages/HomePage";
 
-const router = createBrowserRouter([{ path: "/", element: <AuthPage /> }]);
+const router = createBrowserRouter([
+  { path: "/", element: <AuthPage /> },
+  { path: "/home", element: <HomePage /> },
+]);
 
 function App() {
   return <RouterProvider router={router} />;

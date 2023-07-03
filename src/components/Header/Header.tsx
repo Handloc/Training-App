3087bb29d3618340c42c44cf5ed2dbbb413e09a4
Maciej Handke <maciejhandke11@gmail.com>
2023-07-03 22:37:45 +0200

    Add title and logout button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,12 @@
 const Header: React.FC<{}> = () => {
-  return <></>;
+  return (
+    <div className="flex items-center justify-center bg-zinc-700 p-3">
+      <p className="text-amber-400 text-3xl font-bold">Training App</p>
+      <p className="absolute right-5 text-amber-400 font-bold cursor-pointer hover:text-fuchsia-500 transition-all">
+        Logout
+      </p>
+    </div>
+  );
 };
 
 export default Header;

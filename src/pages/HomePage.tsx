303b45252204f99d50b8e8aeb5959f27233cfea4
Maciej Handke <maciejhandke11@gmail.com>
2023-07-03 22:38:13 +0200

    Check if user is logged in, if not - display error

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,21 @@
 import Header from "../components/Header/Header";
+import { useSelector } from "react-redux";
+import { RootState } from "../store";
 
 const HomePage: React.FC<{}> = () => {
-  return <Header />;
+  const isAuthenticated = useSelector(
+    (state: RootState) => state.auth.isAuthenticated
+  );
+
+  if (isAuthenticated) {
+    return <Header />;
+  } else {
+    return (
+      <p className="flex text-center items-center justify-center m-auto mt-80 w-4/5 text-red-600 text-2xl font-bold">
+        ERROR: User is not logged in!
+      </p>
+    );
+  }
 };
 
 export default HomePage;

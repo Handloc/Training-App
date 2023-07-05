52cfe5b151e0ee262c31e8a4e7c8d11d08c55727
Maciej Handke <maciejhandke11@gmail.com>
2023-07-05 23:08:34 +0200

    Add logout handler

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,20 @@
+import { useDispatch } from "react-redux";
+import { authActions } from "../../store/auth-slice";
+
 const Header: React.FC<{}> = () => {
+  const dispatch = useDispatch<any>();
+
+  const logoutHandler = () => {
+    dispatch(authActions.logout());
+  };
+
   return (
     <div className="flex items-center justify-center bg-zinc-700 p-3">
       <p className="text-amber-400 text-3xl font-bold">Training App</p>
-      <p className="absolute right-5 text-amber-400 font-bold cursor-pointer hover:text-fuchsia-500 transition-all">
+      <p
+        onClick={logoutHandler}
+        className="absolute right-5 text-amber-400 font-bold cursor-pointer hover:text-fuchsia-500 transition-all"
+      >
         Logout
       </p>
     </div>

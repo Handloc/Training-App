0b39878166192e77a9c18c9c1f3fb9f95283eacf
Maciej Handke <maciejhandke11@gmail.com>
2023-07-30 22:51:24 +0200

    Add styled logout button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -19,12 +19,12 @@ const Header: React.FC<{}> = () => {
     <>
       <div className="flex items-center justify-center bg-zinc-700 p-3 pb-0">
         <p className="text-amber-400 text-3xl font-bold">Training App</p>
-        <p
+        <button
+          className="bg-amber-400 hover:bg-fuchsia-600 text-black font-bold  rounded-3xl transition-all absolute right-14 mt-4 p-1 pl-5 pr-5"
           onClick={logoutHandler}
-          className="absolute right-5 text-amber-400 font-bold cursor-pointer hover:text-fuchsia-500 transition-all"
         >
-          Logout
-        </p>
+          <p className="  font-bold cursor-pointe transition-al">Logout</p>
+        </button>
       </div>
       <div className="flex items-center justify-center bg-zinc-700">
         <p className="text-amber-400 text-md font-bold">{currentUser}</p>
@@ -32,5 +32,5 @@ const Header: React.FC<{}> = () => {
     </>
   );
 };
-
+//
 export default Header;

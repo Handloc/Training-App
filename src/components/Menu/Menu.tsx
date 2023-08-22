1e2caa82dbd4320352aa5ed65b4e9f5bb3d2ca38
Maciej Handke <maciejhandke11@gmail.com>
2023-08-22 22:44:54 +0200

    Render given links

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,13 +1,15 @@
-const Menu: React.FC<{}> = () => {
+const Menu: React.FC<{
+  headerLinks: {
+    linkName: string;
+    linkTo: string;
+  }[];
+}> = ({ headerLinks }) => {
   return (
     <div className="bg-[#222831] absolute m-0 top-0 left-0 w-full h-full text-white text-3xl flex flex-col items-center z-[1]">
       <div className="absolute top-40">
-        <p>MY PROFILE</p>
-        <p>MY WORKOUT</p>
-        <p>EXERCISES</p>
-        <p>TRAININGS</p>
-        <p>NUTRITION</p>
-        <p>ENCYCLOPEDIA</p>
+        {headerLinks.map((link) => {
+          return <p>{link.linkName.toUpperCase()}</p>;
+        })}
       </div>
     </div>
   );

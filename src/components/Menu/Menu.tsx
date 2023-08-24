66cd87edfbd70f210123365f048af31ca4f71e19
Maciej Handke <maciejhandke11@gmail.com>
2023-08-24 22:38:10 +0200

    Dispatch logout

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,16 +1,31 @@
+import { useDispatch } from "react-redux";
+import { RootState } from "../../store";
+import { ThunkDispatch } from "redux-thunk";
+import { authActions } from "../../store/auth-slice";
+
 const Menu: React.FC<{
   headerLinks: {
     linkName: string;
     linkTo: string;
   }[];
 }> = ({ headerLinks }) => {
+  const dispatch = useDispatch<ThunkDispatch<RootState, undefined, any>>();
+
   return (
-    <div className="bg-[#222831] absolute m-0 top-0 left-0 w-full h-full text-white text-3xl flex flex-col items-center z-[1]">
-      <div className="absolute top-40">
+    <div className="bg-[#222831] absolute m-0 top-0 left-0 w-full h-full text-white text-3xl flex flex-col items-center justify-center z-[1]">
+      <div className="flex flex-col justify-center items-center">
         {headerLinks.map((link) => {
-          return <p>{link.linkName.toUpperCase()}</p>;
+          return <p className=" pb-16">{link.linkName.toUpperCase()}</p>;
         })}
       </div>
+      <button
+        className="bg-[#F05454] hover:bg-[#30475E] hover:shadow-md text-white mt-10 p-4 px-16 rounded-md transition-all duration-200 text-2xl"
+        onClick={() => {
+          dispatch(authActions.logout());
+        }}
+      >
+        Logout
+      </button>
     </div>
   );
 };

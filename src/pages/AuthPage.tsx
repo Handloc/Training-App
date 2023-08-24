ff9316260ac590a14b1dd7830ca2b855a3a5b139
Maciej Handke <maciejhandke11@gmail.com>
2023-08-24 23:05:36 +0200

    Display Home Page if authenticated, if not - show authentication form

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -1,11 +1,24 @@
 import AuthForm from "../components/AuthForm/AuthForm";
 import Header from "../components/Header/Header";
+import { useSelector } from "react-redux";
+import { RootState } from "../store";
+import HomePage from "./HomePage";
 
 const AuthPage: React.FC<{}> = () => {
+  const isAuthenticated = useSelector(
+    (state: RootState) => state.auth.isAuthenticated
+  );
+
   return (
     <>
-      <Header />
-      <AuthForm />
+      {isAuthenticated ? (
+        <HomePage />
+      ) : (
+        <>
+          <Header />
+          <AuthForm />
+        </>
+      )}
     </>
   );
 };

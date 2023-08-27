f022d98532389c9b1b39d103027c394f5080758f
Maciej Handke <maciejhandke11@gmail.com>
2023-08-27 20:18:29 +0200

    Redirect if logged in

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -2,18 +2,23 @@ import AuthForm from "../components/AuthForm/AuthForm";
 import Header from "../components/Header/Header";
 import { useSelector } from "react-redux";
 import { RootState } from "../store";
-import HomePage from "./HomePage";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const AuthPage: React.FC<{}> = () => {
   const isAuthenticated = useSelector(
     (state: RootState) => state.auth.isAuthenticated
   );
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate("/home");
+    }
+  }, [isAuthenticated, navigate]);
 
   return (
     <>
-      {isAuthenticated ? (
-        <HomePage />
-      ) : (
+      {isAuthenticated && (
         <>
           <Header />
           <AuthForm />

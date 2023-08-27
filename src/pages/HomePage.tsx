8a8b7e998978e9bc4e735c96639585b28bd5f88c
Maciej Handke <maciejhandke11@gmail.com>
2023-08-27 20:18:58 +0200

    Show error if not logged in

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -7,16 +7,24 @@ const HomePage: React.FC<{}> = () => {
     (state: RootState) => state.auth.isAuthenticated
   );
 
-  // if (isAuthenticated) {
-  //   return <Header />;
-  // } else {
-  //   return (
-  //     <p className="flex text-center items-center justify-center m-auto mt-80 w-4/5 text-red-600 text-2xl font-bold">
-  //       ERROR: User is not logged in!
-  //     </p>
-  //   );
-  // }
-  return <Header />;
+  if (isAuthenticated) {
+    return (
+      <>
+        <Header />
+        <div className="flex flex-col justify-center items-center">
+          <div className="bg-slate-600 text-white text-lg mt-10 w-4/5 py-10 rounded-md transition-all duration-200 flex flex-col items-center justify-center">
+            CURRENT TRAINING
+          </div>
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <p className="flex text-center items-center justify-center m-auto mt-80 w-4/5 text-red-600 text-2xl font-bold">
+        ERROR: User is not logged in!
+      </p>
+    );
+  }
 };
 
 export default HomePage;

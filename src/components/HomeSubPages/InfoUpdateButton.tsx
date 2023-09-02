9d3c0b700981d0f545e09fe4ad37d5acd019d06b
Maciej Handke <maciejhandke11@gmail.com>
2023-09-02 14:34:24 +0200

    Add dispatching of an action

--- src/components/HomeSubPages/InfoUpdateButton.tsx
+++ src/components/HomeSubPages/InfoUpdateButton.tsx
@@ -1,8 +1,18 @@
+import { useDispatch, useSelector } from "react-redux";
+import { modalActions } from "../../store/modal-slice";
+
 const InfoUpdateButton: React.FC<{ displayName: string }> = ({
   displayName,
 }) => {
+  const dispatch = useDispatch();
+
   return (
-    <button className="bg-[#F05454] hover:bg-[#30475E] hover:shadow-md text-white rounded-md transition-all duration-200 text-lg mt-4 w-full mx-4 p-2 text-center cursor-pointer">
+    <button
+      onClick={() => {
+        dispatch(modalActions.showHomePageModal());
+      }}
+      className="bg-[#F05454] hover:bg-[#30475E] hover:shadow-md text-white rounded-md transition-all duration-200 text-lg mt-4 w-full mx-4 p-2 text-center cursor-pointer"
+    >
       {displayName}
     </button>
   );

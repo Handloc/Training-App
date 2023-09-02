e59e86fb476e45abeed84647bbc07625a905750c
Maciej Handke <maciejhandke11@gmail.com>
2023-09-02 12:33:32 +0200

    Create button component

---
+++ src/components/HomeSubPages/GraphButton.tsx
@@ -0,0 +1,14 @@
+import { NavLink } from "react-router-dom";
+
+const GraphButton: React.FC<{ linkTo: string; displayName: string }> = ({
+  linkTo,
+  displayName,
+}) => {
+  return (
+    <div className="bg-[#F05454] hover:bg-[#30475E] hover:shadow-md text-white rounded-md transition-all duration-200 text-lg mt-4 w-full p-4 text-center">
+      <NavLink to={linkTo}>{displayName}</NavLink>
+    </div>
+  );
+};
+
+export default GraphButton;

854389406f83342f5b0b78a09478d2ea13620c2a
Maciej Handke <maciejhandke11@gmail.com>
2023-09-02 14:35:44 +0200

    Add Modal reducer into Redux store

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth-slice";
 import usersReducer from "./users-slice";
+import modalRecuder from "./modal-slice";
 
 const store = configureStore({
-  reducer: { auth: authReducer, users: usersReducer },
+  reducer: { auth: authReducer, users: usersReducer, modal: modalRecuder },
 });
 
 export type RootState = ReturnType<typeof store.getState>;

b247faff6e11410501c3bb36d11629773de27fdf
Maciej Handke <maciejhandke11@gmail.com>
2023-09-02 13:10:23 +0200

    Add cursor-pointer

--- src/components/HomeSubPages/GraphButton.tsx
+++ src/components/HomeSubPages/GraphButton.tsx
@@ -5,7 +5,7 @@ const GraphButton: React.FC<{ linkTo: string; displayName: string }> = ({
   displayName,
 }) => {
   return (
-    <div className="bg-[#F05454] hover:bg-[#30475E] hover:shadow-md text-white rounded-md transition-all duration-200 text-lg mt-4 w-full p-4 text-center">
+    <div className="bg-[#F05454] hover:bg-[#30475E] hover:shadow-md text-white rounded-md transition-all duration-200 text-lg mt-4 w-full p-4 text-center cursor-pointer">
       <NavLink to={linkTo}>{displayName}</NavLink>
     </div>
   );

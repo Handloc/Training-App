e81ccc1d66e7337490eeef88e0a1ec530568f80d
Maciej Handke <maciejhandke11@gmail.com>
2023-09-03 18:09:38 +0200

    Dispatch an action

--- src/components/HomeSubPages/InfoUpdateButton.tsx
+++ src/components/HomeSubPages/InfoUpdateButton.tsx
@@ -10,6 +10,7 @@ const InfoUpdateButton: React.FC<{ displayName: string }> = ({
     <button
       onClick={() => {
         dispatch(modalActions.showHomePageModal());
+        dispatch(modalActions.setCurrentModal(displayName));
       }}
       className="bg-[#F05454] hover:bg-[#30475E] hover:shadow-md text-white rounded-md transition-all duration-200 text-lg mt-4 w-full mx-4 p-2 text-center cursor-pointer"
     >

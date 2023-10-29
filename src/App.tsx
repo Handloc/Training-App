c82a0c49088e123b1da1e1d43d5f3605a2e98953
Maciej Handke <maciejhandke11@gmail.com>
2023-10-29 18:22:13 +0100

    Add MSW into project

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,11 @@ const router = createBrowserRouter([
   { path: "/home", element: <HomePage /> },
 ]);
 
+if (process.env.NODE_ENV === "development") {
+  const { worker } = require("./mocks/browser");
+  worker.start();
+}
+
 function App() {
   return <RouterProvider router={router} />;
 }
